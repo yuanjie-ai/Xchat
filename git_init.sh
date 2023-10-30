4a2d9daab41e756f9eb57cfb112371239bb74785
Betterme <313303303@qq.com>
2023-10-30 14:35:53 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -6,5 +6,5 @@ git commit -m "init"
 
 git remote add origin git@github.com:yuanjie-ai/xchat.git
 git branch -M master
-git push -u origin master
+git push -u origin master -f
 # git remote remove origin
